76bebd226a0d6fd3e7e9a45775bc2c89051a2f38
Kevin Chen <kevinmichaelchen@gmail.com>
2019-01-21 08:38:10 -0500

    Implement move operation

--- move.go
+++ move.go
@@ -68,5 +68,38 @@ func (s *MoveService) Move(w http.ResponseWriter, r *http.Request) {
 		// TODO validate new name is not too long; check if it's actually different
 	}
 
+	// Delete the old CONTAINS_FILE relationship
+	tx, err := session.BeginTransaction()
+	_, err = tx.Run(`MATCH (folder:Folder { resource_id: $old_parent_id })-[r:CONTAINS_FILE]->(file:File { resource_id: $file_id }) DELETE r`,
+		map[string]interface{}{"file_id": source.ResourceID.String(), "old_parent_id": source.ParentID.String()})
+
+	// Rollback if there's an error
+	if err != nil {
+		err = tx.Rollback()
+		if err != nil {
+			requestUtils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+	}
+
+	// Create a new CONTAINS_FILE relationship
+	_, err = tx.Run(`MATCH (folder:Folder), (file:File) WHERE folder.resource_id = $new_parent_id AND file.resource_id = $file_id CREATE (folder)-[r:CONTAINS_FILE]->(file) RETURN type(r)`,
+		map[string]interface{}{"file_id": source.ResourceID.String(), "new_parent_id": dest.ResourceID.String()})
+
+	// Rollback if there's an error
+	if err != nil {
+		err = tx.Rollback()
+		if err != nil {
+			requestUtils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		requestUtils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
 	requestUtils.RespondWithJSON(w, http.StatusOK, map[string]string{"msg": "Success"})
 }
